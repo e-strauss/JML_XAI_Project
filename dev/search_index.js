6273dbf1766b3d216d2e982e985b59f63c1bdd48
Documenter.jl <documenter@juliadocs.github.io>
2024-05-31 08:38:11 +0000

    build based on bb967d9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JML_XAI_Project","category":"page"},{"location":"#JML*XAI*Project","page":"Home","title":"JMLXAIProject","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JMLXAIProject.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JML_XAI_Project]","category":"page"}]
+}

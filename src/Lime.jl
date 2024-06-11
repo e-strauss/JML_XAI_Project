4c4c11cf4e2d3063461bb72393fc6b41b83a3a53
Elias Strauß <lathancer@gmx.de>
2024-06-11 12:48:08 +0200

    added details in function description (#23)
    * added details in function description
    
    * reference to paper

--- src/Lime.jl
+++ src/Lime.jl
@@ -5,6 +5,8 @@ struct LIME{M} <: AbstractXAIMethod
     model::M    
 end
 
+
+#implementation of LIME XAI method based on https://arxiv.org/pdf/1602.04938
 function (method::LIME)(input, output_selector::AbstractOutputSelector)
     output = method.model(input)
     output_selection = output_selector(output)
@@ -14,7 +16,7 @@ function (method::LIME)(input, output_selector::AbstractOutputSelector)
     kernel_fn = identity
     max_features = length(input)
     distances = [0]
-    #TODO: use Lime-base for explanation
+    #TODO:  replace "dummy" values with meaniningful values
     #       - create actual samples around the point input, compute labels and distances 
     #       - specify a kernel function that transforms an array of distances into an array of proximity values
     #       - specify a maximum num feature of features for the simpliefied model

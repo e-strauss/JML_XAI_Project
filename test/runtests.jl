fd9c070684514dc7e9b41c641e84c2310225eedf
e-strauss <92718421+e-strauss@users.noreply.github.com>
2024-06-25 15:39:35 +0200

    reduced dependencies

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,8 @@ using Test
 
 @testset "JML_XAI_Project.jl" begin
     #sanity check on the getting started tutorial
+    #include("getting-started-test.jl")
+    
     include("lime_feature_selection_test.jl")
-    include("getting-started-test.jl")
     include("lime_xai_test.jl")
 end

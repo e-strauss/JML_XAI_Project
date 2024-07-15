bfe3c87b253167d1b9cf797c7d7e0d90ed97dd6a
jon-kri <j.kriegs@t-online.de>
2024-07-15 11:19:02 +0200

    SHAP finished

--- src/Lime.jl
+++ src/Lime.jl
@@ -3,10 +3,14 @@ include("Lime-base.jl")
 include("Lime-image.jl")
 include("SHAP-Kernel.jl")
 
-struct LIME{M} <: AbstractXAIMethod 
-    model::M    
+struct LIME{M, K, l} <: AbstractXAIMethod 
+    model::M
+    kernel::K
+    lasso::l
+
 end
 
+LIME(M) = LIME(M, exponential_kernel, true)
 
 #implementation of LIME XAI method based on https://arxiv.org/pdf/1602.04938
 function (method::LIME)(input, output_selector::AbstractOutputSelector)
@@ -16,7 +20,7 @@ function (method::LIME)(input, output_selector::AbstractOutputSelector)
     #select first input
     input = input[:,:,:,1]
 
-    val = explain_instance(input, method.model, output_selection[1])
+    val = explain_instance(input, method.model, output_selection[1]; kernel=method.kernel, lasso=method.lasso)
     extras = nothing
     return Explanation(val, output, output_selection[1], :MyMethod, :attribution, extras)
 end
